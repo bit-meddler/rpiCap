09870d47177986bc95657a2a68a67198687d7078
RichH <RichH@beacon-optical.co.uk>
2017-10-30 16:00:31 +0000

    Tkinter treeview, messing with selection highlighting

--- sketches/testTkinter.py
+++ sketches/testTkinter.py
@@ -1,5 +1,6 @@
 from Tkinter import *
 import ttk
+# import OpenGL.Tk
 
 ORDER = {   "cam_list": 0,
             "syn_list": 1,
@@ -15,8 +16,29 @@ root = Tk()
 
 tree = ttk.Treeview( root, show=['tree'] )
 
+def onSelect( event ):
+    # not good, only a test
+    global tree
+    sel_list = tree.selection()
+    print sel_list
+    for itm in sel_list:
+        old_tag = tree.item( itm, "tag" )[0]
+        if old_tag.startswith( "default" ):
+            if old_tag.endswith( "h" ):
+                tree.item( itm, tag="default" )
+            else:
+                tree.item( itm, tag="default_h" )
+        else:
+            if old_tag.endswith( "h" ):
+                tree.item( itm, tag="disabled" )
+            else:
+                tree.item( itm, tag="disabled_h" )
+        
 tree.tag_configure( "default",  font=('Helvetica', 10) )
 tree.tag_configure( "disabled", font=('Helvetica', 10, 'italic') )
+tree.tag_configure( "default_h",  font=('Helvetica', 10, 'bold') )
+tree.tag_configure( "disabled_h", font=('Helvetica', 10, 'bold italic') )
+
 
 for k in ORDER.keys():
     tree.insert( "", ORDER[k], k, text=NAMES[k], tag="default" )
@@ -26,12 +48,14 @@ tree.insert( "cam_list", "end", "cam_2", text="Camera 2", tag="default" )
 tree.insert( "cam_list", "end", "cam_3", text="Camera 3", tag="default" )
 tree.insert( "cam_list", "end", "cam_4", text="Camera 4", tag="default" )
 
-tree.insert( "syn_list", 1, text="Sync Box", tag="default" )
+tree.insert( "syn_list", 1, "sync_1", text="Sync Box", tag="default" )
 
 # make unpopulated headings italic
 tree.item( "tri_cams", tag="disabled" )
 tree.item( "tri_hmcs", tag="disabled" )
 tree.item( "tri_plam", tag="disabled" )
+
+tree.bind("<ButtonRelease-1>", onSelect )
 
 tree.pack()
 root.mainloop()
