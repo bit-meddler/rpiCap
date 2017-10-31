067f378d44e2906dbba0c518286d0157dcd0c43a
RichH <RichH@beacon-optical.co.uk>
2017-10-31 08:41:39 +0000

    Treeview. bitmasks for tag twiddling

--- sketches/testTkinter.py
+++ sketches/testTkinter.py
@@ -2,11 +2,11 @@ from Tkinter import *
 import ttk
 # import OpenGL.Tk
 
-ORDER = {   "cam_list": 0,
-            "syn_list": 1,
-            "tri_cams": 2,
-            "tri_hmcs": 3,
-            "tri_plam": 4 }
+ORDER = {   "cam_list":  0,
+            "syn_list": 10,
+            "tri_cams": 200,
+            "tri_hmcs": 300,
+            "tri_plam": 400 }
 NAMES = {   "cam_list": "Cameras",
             "syn_list": "Sync Devices",
             "tri_cams": "Triggered Camcorders",
@@ -16,6 +16,10 @@ root = Tk()
 
 tree = ttk.Treeview( root, show=['tree'] )
 
+TAG_DEF  = 0x0
+TAG_SEL  = 0x1
+TAG_DIS  = 0x2
+
 def onSelect( event ):
     # not good, only a test
     global tree
@@ -23,39 +27,30 @@ def onSelect( event ):
     print sel_list
     for itm in sel_list:
         old_tag = tree.item( itm, "tag" )[0]
-        if old_tag.startswith( "default" ):
-            if old_tag.endswith( "h" ):
-                tree.item( itm, tag="default" )
-            else:
-                tree.item( itm, tag="default_h" )
-        else:
-            if old_tag.endswith( "h" ):
-                tree.item( itm, tag="disabled" )
-            else:
-                tree.item( itm, tag="disabled_h" )
+        # Bit Twiddle!
         
-tree.tag_configure( "default",  font=('Helvetica', 10) )
-tree.tag_configure( "disabled", font=('Helvetica', 10, 'italic') )
-tree.tag_configure( "default_h",  font=('Helvetica', 10, 'bold') )
-tree.tag_configure( "disabled_h", font=('Helvetica', 10, 'bold italic') )
+tree.tag_configure( TAG_DEF,         font=('Helvetica', 10) )
+tree.tag_configure( TAG_DIS,         font=('Helvetica', 10, 'italic') )
+tree.tag_configure( TAG_SEL,         font=('Helvetica', 10, 'bold') )
+tree.tag_configure( TAG_DIS|TAG_SEL, font=('Helvetica', 10, 'bold italic') )
 
 
 for k in ORDER.keys():
-    tree.insert( "", ORDER[k], k, text=NAMES[k], tag="default" )
+    tree.insert( "", ORDER[k], k, text=NAMES[k], tag=TAG_DEF )
 
-tree.insert( "cam_list", "end", "cam_1", text="Camera 1", tag="default" )
-tree.insert( "cam_list", "end", "cam_2", text="Camera 2", tag="default" )
-tree.insert( "cam_list", "end", "cam_3", text="Camera 3", tag="default" )
-tree.insert( "cam_list", "end", "cam_4", text="Camera 4", tag="default" )
+tree.insert( "cam_list", "end", "cam_1", text="Camera 1", tag=TAG_DEF )
+tree.insert( "cam_list", "end", "cam_2", text="Camera 2", tag=TAG_DEF )
+tree.insert( "cam_list", "end", "cam_3", text="Camera 3", tag=TAG_DEF )
+tree.insert( "cam_list", "end", "cam_4", text="Camera 4", tag=TAG_DEF )
 
-tree.insert( "syn_list", 1, "sync_1", text="Sync Box", tag="default" )
+tree.insert( "syn_list", 1, "sync_1", text="Sync Box", tag=TAG_DEF )
 
 # make unpopulated headings italic
-tree.item( "tri_cams", tag="disabled" )
-tree.item( "tri_hmcs", tag="disabled" )
-tree.item( "tri_plam", tag="disabled" )
+tree.item( "tri_cams", tag=TAG_DIS )
+tree.item( "tri_hmcs", tag=TAG_DIS )
+tree.item( "tri_plam", tag=TAG_DIS )
 
-tree.bind("<ButtonRelease-1>", onSelect )
+tree.bind(" <ButtonRelease-1>", onSelect )
 
 tree.pack()
 root.mainloop()
