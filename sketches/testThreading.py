5768dfc4cd59689e5e56896cceb751b556a587da
RichH <RichH@beacon-optical.co.uk>
2017-11-08 13:40:33 +0000

    Serial idea

--- sketches/testThreading.py
+++ sketches/testThreading.py
@@ -5,9 +5,9 @@ import threading
 from multiprocessing import Process, Queue, Pool
 import time
 
-class compute( object ):
-""" Dummy class to pretend to do a costly function
-"""
+class Compute( object ):
+    """ Dummy class to pretend to do a costly function
+    """
 
     def __init__( self, private_data, config_data, id ):
         self.private_data = private_data
@@ -15,13 +15,19 @@ class compute( object ):
         self.id = id
         
     def push( self, input ):
-        time.sleep(2)
-        print "Executing with {} & {} & {} on {}".format(
-            self.private_data, self, config_data, input, self.id
+        time.sleep(1)
+        print "Executing with {} & {} on {} @ {}".format(
+            self.private_data, self.config_data, input, self.id
         )
-        time.sleep(2)
+        time.sleep(1)
         return ( self.id, input )
         
         
 if __name__ == "__main__":
-    # Hello!
+    num_cores = 4
+    # Serial
+    imperatives = [ "egg", "bacon", "beans", "spam", "lobster thermidor" ]
+    task_list = [ Compute( "spam", imperatives[i], i ) for i in range( num_cores ) ]
+
+    for task in task_list:
+        print( task.push( "egg" ) )
