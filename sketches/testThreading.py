5291fcfdf21cc9c7db17cd39aed43b5498b29f69
RichH <RichH@beacon-optical.co.uk>
2017-11-08 17:10:10 +0000

    A threading Test

--- sketches/testThreading.py
+++ sketches/testThreading.py
@@ -14,20 +14,54 @@ class Compute( object ):
         self.config_data = config_data
         self.id = id
         
-    def push( self, input ):
+    def push( self, input, output=None ):
         time.sleep(1)
         print "Executing with {} & {} on {} @ {}".format(
             self.private_data, self.config_data, input, self.id
         )
         time.sleep(1)
-        return ( self.id, input )
         
+        ret = ( self.id, time.time() )
+        
+        if( output == None ):
+            return ret
+        else:
+            output.put( ret )
+            
         
 if __name__ == "__main__":
     num_cores = 4
     # Serial
     imperatives = [ "egg", "bacon", "beans", "spam", "lobster thermidor" ]
-    task_list = [ Compute( "spam", imperatives[i], i ) for i in range( num_cores ) ]
+    func_list = [ Compute( "spam", imperatives[i], i ) for i in range( num_cores ) ]
 
-    for task in task_list:
-        print( task.push( "egg" ) )
+    res_list = [None] * num_cores
+    proc_list = []
+    ret_q = Queue()
+    
+    t1 = time.time()
+
+    data = "Bacon!"
+    for func in func_list:
+        worker = threading.Thread( target=func.push, args=(data,ret_q,) )
+        worker.start()
+        proc_list.append( worker )
+    
+    for proc in proc_list:
+        proc.join()
+    
+    t2 = time.time()
+    
+    for i in range(num_cores):
+        idx, res =  ret_q.get()
+        res_list[ idx ] = res
+
+    print "Total", t2-t1
+    
+    for i, res in enumerate( res_list ):
+        print i, res
+
+    exit()
+    for task in func_list:
+        idx, res =  task.push( "egg" )
+        res_list[ idx ] = res
