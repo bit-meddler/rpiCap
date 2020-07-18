b4152c8ed5c482d236a6398381836fafb3dcb5ca
bit-meddler <>
2020-07-18 16:06:29 +0100

    Couldn't help myself, had to add a timer to emit dummy centroids

--- camSim/regsResponder.py
+++ camSim/regsResponder.py
@@ -5,8 +5,11 @@ sys.path.append( os.path.join( _git_root_, "midget", "Python" ) )
 
 
 import numpy as np # It's started....
+import select
 import socket
 import struct
+import threading
+import time
 
 from Comms import piCam
 
@@ -16,17 +19,38 @@ UDP_PORT_RX    = 1235  # Server's TX
 SOCKET_TIMEOUT = 10
 RECV_BUFF_SZ   = 10240 # bigger than a Jumbo Frame
 
-sock = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
-sock.setsockopt( socket.SOL_SOCKET, socket.SO_REUSEADDR, 1 )
-sock.bind( (SERVER_IP, UDP_PORT_TX) )
+coms_socket = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
+coms_socket.setsockopt( socket.SOL_SOCKET, socket.SO_REUSEADDR, 1 )
+coms_socket.bind( (SERVER_IP, UDP_PORT_TX) )
 
 CAMERA_ID = SERVER_IP.split(".")[-1]
 
 print( "Dawson up to the plate, a beautiful day here at Wrigley Field..." )
 
 # Listen for incoming message and just repeat them back
-running = True
+class Metronome( threading.Thread ):
+    
+    def __init__( self, the_flag, delay ):
+        super( Metronome, self ).__init__()
+        self._flag = the_flag
+        self._delay = delay
+        self.running = threading.Event()
+        self.running.set()
+        
+    def run( self ):
+        while self.running.isSet():
+            time.sleep( self._delay )
+            self._flag.set()
 
+    def stop( self ):
+        self.running.clear()
+        
+have_roids = threading.Event()
+have_roids.clear()
+
+TEST_PERIOD = 3.0 # 3 secs
+
+# camera state
 myRegs = np.zeros( (823,), dtype=np.uint8 )
 
 # some internals for counting packets
@@ -72,77 +96,87 @@ def packetize( dtype, data, address ):
 def makeLog( msg ):
     return "*** CAMERA {} {} ***".format( CAMERA_ID, msg ).encode( "ascii" )
 
-sim_roids = False # are we simulating sending Centroids?
+
+inputs = [coms_socket,]
+timer = None
+running = True
 while( running ):
+    readable, _, _ = select.select( inputs, [], [], 0 )
+    for sock in readable:
+        data, address = sock.recvfrom( RECV_BUFF_SZ )
 
-    data, address = sock.recvfrom( RECV_BUFF_SZ )
+        #print( "Message from {}:'{}'".format( address, data ) )
 
-    #print( "Message from {}:'{}'".format( address, data ) )
+        if( data == b"bye" ): # Quick exit
+            running = False
+            continue
+        
+        # understand the data
+        sz = len( data )
+        if( sz == 1 ):
+            if( data in piCam.CAMERA_COMMANDS_REV ):
+                cmd = piCam.CAMERA_COMMANDS_REV[ data ]
+                if( cmd == "start" ):
+                    timer = Metronome( have_roids, TEST_PERIOD )
+                    timer.start()
+                    
+                elif( cmd == "stop" ):
+                    if( timer is not None ):
+                        timer.stop()
+                        timer = None
+                elif( cmd == "single" ):
+                    # Send a single image
+                    pass
+                
+                elif( cmd == "stream" ):
+                    # Stream Images
+                    pass
+                # report
+                packetize( piCam.PACKET_TYPES[ "textslug" ], makeLog( cmd ), address )
+                
+            elif( data in piCam.CAMERA_REQUESTS_REV ):
+                req = piCam.CAMERA_REQUESTS_REV[ data ]
+                if( req == "regslo" ):
+                    packetize( piCam.PACKET_TYPES[ "regslo" ], myRegs[:56].tobytes(), address )
+                    
+                elif( req == "regshi" ):
+                    packetize( piCam.PACKET_TYPES[ "regshi" ], myRegs.tobytes(), address )
+                    
+                elif( req == "version" ):
+                    packetize( piCam.PACKET_TYPES[ "version" ], b"DEADBEEEF", address )
+                    
+                elif( req == "hello" ):
+                    packetize( piCam.PACKET_TYPES[ "textslug" ], b"h", address )
 
-    if( data == b"bye" ): # Quick exit
-        running = False
-        continue
-    
-    # understand the data
-    sz = len( data )
-    if( sz == 1 ):
-        if( data in piCam.CAMERA_COMMANDS_REV ):
-            cmd = piCam.CAMERA_COMMANDS_REV[ data ]
-            if( cmd == "start" ):
-                sim_roids = True
+        else:
+            if( data[0] == piCam.CHAR_COMMAND_VAL ):
+                # low reg command
+                _, reg, val = struct.unpack( ">BBB", data )
+                myRegs[ reg-200 ] = val
                 
-            elif( cmd == "stop" ):
-                sim_roids = False
+                # report
+                msg = makeLog( "Register {} Set to {}".format( reg, val ) )
+                packetize( piCam.PACKET_TYPES[ "textslug" ], msg, address )
                 
-            elif( cmd == "single" ):
-                # Send a single image
-                pass
-            
-            elif( cmd == "stream" ):
-                # Stream Images
-                pass
-            # report
-            packetize( piCam.PACKET_TYPES[ "textslug" ], makeLog( cmd ), address )
-            
-        elif( data in piCam.CAMERA_REQUESTS_REV ):
-            req = piCam.CAMERA_REQUESTS_REV[ data ]
-            if( req == "regslo" ):
-                packetize( piCam.PACKET_TYPES[ "regslo" ], myRegs[:56].tobytes(), address )
+            if( data[0] == piCam.SHORT_COMMAND_VAL ):
+                # High Reg Command
+                _, reg, msb, lsb = struct.unpack( ">BHBB", data )
+                _, _, val = struct.unpack( ">BHh", data )
                 
-            elif( req == "regshi" ):
-                packetize( piCam.PACKET_TYPES[ "regshi" ], myRegs.tobytes(), address )
+                myRegs[ reg-200 ] = msb
+                myRegs[ reg-199 ] = lsb # or reg + 1 :D
                 
-            elif( req == "version" ):
-                packetize( piCam.PACKET_TYPES[ "version" ], b"DEADBEEEF", address )
-                
-            elif( req == "hello" ):
-                packetize( piCam.PACKET_TYPES[ "textslug" ], b"h", address )
-
-    else:
-        if( data[0] == piCam.CHAR_COMMAND_VAL ):
-            # low reg command
-            _, reg, val = struct.unpack( ">BBB", data )
-            myRegs[ reg-200 ] = val
+                # report
+                msg = makeLog( "Register {} Set to {}".format( reg, val ) )
+                packetize( piCam.PACKET_TYPES[ "textslug" ], msg, address )
             
-            # report
-            msg = makeLog( "Register {} Set to {}".format( reg, val ) )
-            packetize( piCam.PACKET_TYPES[ "textslug" ], msg, address )
-            
-        if( data[0] == piCam.SHORT_COMMAND_VAL ):
-            # High Reg Command
-            _, reg, msb, lsb = struct.unpack( ">BHBB", data )
-            _, _, val = struct.unpack( ">BHh", data )
-            
-            myRegs[ reg-200 ] = msb
-            myRegs[ reg-199 ] = lsb # or reg + 1 :D
-            
-            # report
-            msg = makeLog( "Register {} Set to {}".format( reg, val ) )
-            packetize( piCam.PACKET_TYPES[ "textslug" ], msg, address )
-            
-    if( sim_roids ):
+    if( have_roids.isSet() ):
         # Connected components, Hu moments, Quantize, and a Partridge in a Pear Tree!
         print( "Sim Centroids" )
+        have_roids.clear()
+        
+if( timer is not None ):
+    timer.stop()
 
-    
+print( "Sent {} Packets this session".format( packet_num ) )
 print( "What do you think, Sirs?" )
