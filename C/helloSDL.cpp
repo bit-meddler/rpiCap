1bdd287b661250f3400ce2772e117fece038111c
bit-meddler <RichH@Beacon-Optical.co.uk>
2020-09-26 07:43:01 +0100

    UNTESTED! implemented pixel skipping in NEON, however my pi died so I rescued the code from the SD and roughed it in on a x64 PC and here it is

--- C/helloSDL.cpp
+++ C/helloSDL.cpp
@@ -1,84 +1,90 @@
 #include <stdio.h>
 #include <string>
+#include "time.h"
 #include "SDL2/SDL.h"
 #include "vision.cpp"
 
 
-int main( int argc, char* args[] ) {
-	// Just your average typical first compiled program
-	printf("Hello, world!\n" ) ;
+timespec TimeDelta( timespec start, timespec end )
+{
+    timespec delta ;
 
-	int a, b, c ;
-	a = 2 ;
-	b = 6 ;
-	c = vision::sum( a, b ) ;
-	printf( "%d + %d = %d", a, b, c ) ;
-	
-	// init SDL
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 ) {
-		fprintf( stderr, "ERROR, SDL_Init() - '%s'.\n", SDL_GetError() ) ;
-		return -1 ;
-	}
-	
-	std::string img_path_fq = "/code/rpiCap/benchmarkData/testing_000_0000.bmp" ;
-	SDL_Surface *bmp = SDL_LoadBMP( img_path_fq.c_str() ) ;
-	if( bmp == nullptr ) {
-		fprintf( stderr, "ERROR, SDL_LoadBMP() - '%s'.\n", SDL_GetError() ) ;
-		SDL_Quit() ;
-		return -1 ;
-	}
-	int w = bmp->w ;
-	int h = bmp->h ;
-	Uint16 row = bmp->pitch ;
-	SDL_PixelFormat *pixfmt = bmp->format ;
-	int bpp = pixfmt->BitsPerPixel ;
-	
-	fprintf( stdout, "x:%d, y:%d row:%d, depth: %d.\n", w, h, row, bpp ) ;
-	
-	for( int i=0; i < 16; i++ ) {
-		Uint8 pix = *((Uint8*) bmp->pixels + i) ;
-		printf( "%d, ", pix ) ;
-	}
-	printf("\n") ;
-	
-	SDL_Window *my_win = SDL_CreateWindow( "Hello World!", 100, 100, 540, 380, SDL_WINDOW_SHOWN ) ;
-	if( my_win == nullptr ) {
-		fprintf( stderr, "ERROR, SDL_CreateWindow() - '%s'.\n", SDL_GetError() ) ;
-		SDL_Quit() ;
-		return -1 ;
-	}
-	
-	SDL_Renderer *render = SDL_CreateRenderer( my_win, -1, SDL_RENDERER_SOFTWARE ) ;
-	if( render == nullptr ) {
-		fprintf( stderr, "ERROR, SDL_CreateRenderer() - '%s'.\n", SDL_GetError() ) ;
-		SDL_DestroyWindow( my_win ) ;
-		SDL_Quit() ;
-		return -1 ;
-	}
-	
-	SDL_Texture *tex = SDL_CreateTextureFromSurface( render, bmp ) ;
-	SDL_FreeSurface( bmp ) ;
-	if( tex == nullptr ) {
-		fprintf( stderr, "ERROR, SDL_CreateTextureFromSurface() - '%s'.\n", SDL_GetError() ) ;
-		SDL_DestroyRenderer( render ) ;
-		SDL_DestroyWindow( my_win ) ;
-		SDL_Quit() ;
-		return -1 ;
-	}
-	
-	// Attempt to Draw
-	for( int i=0; i < 3; i++ ) {
-		SDL_RenderClear( render ) ;
-		SDL_RenderCopy( render, tex, NULL, NULL ) ;
-		SDL_RenderPresent( render ) ;
-		
-		SDL_Delay( 1000 ) ;
-	}
-	
-	// the end
-	SDL_DestroyTexture( tex ) ;
-	SDL_DestroyRenderer( render ) ;
-	SDL_DestroyWindow( my_win ) ;
-	SDL_Quit() ;
-	return 1 ;
+    delta.tv_sec  = end.tv_sec  - start.tv_sec  ;
+    delta.tv_nsec = end.tv_nsec - start.tv_nsec ;
+
+    // rolled into secs ?
+    if( delta.tv_nsec  < 0 ) {
+        delta.tv_sec  -= 1 ;
+        delta.tv_nsec += 1000000000 ;
+    }
+
+    return delta ;
+}
+
+
+int main( int argc, char* args[] ) {
+    // Just your average typical first compiled program
+    printf("Testing Connected components !\n" ) ;
+
+    int a, b, c ;
+    a = 2 ;
+    b = 6 ;
+    c = vision::sum( a, b ) ; // have I linked vision correctly?
+    printf( "%d + %d = %d", a, b, c ) ;
+    
+    // init SDL
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 ) {
+        fprintf( stderr, "ERROR, SDL_Init() - '%s'.\n", SDL_GetError() ) ;
+        return -1 ;
+    }
+    
+    std::string img_path_fq = "/code/rpiCap/benchmarkData/testing_000_0000.bmp" ;
+    SDL_Surface *bmp = SDL_LoadBMP( img_path_fq.c_str() ) ;
+    if( bmp == nullptr ) {
+        fprintf( stderr, "ERROR, SDL_LoadBMP() - '%s'.\n", SDL_GetError() ) ;
+        SDL_Quit() ;
+        return -1 ;
+    }
+    int w = bmp->w ;
+    int h = bmp->h ;
+    Uint16 row = bmp->pitch ;
+    SDL_PixelFormat *pixfmt = bmp->format ;
+    int bpp = pixfmt->BitsPerPixel ;
+
+    int num_px = sizeof( *pixfmt ) ;
+    
+    fprintf( stdout,
+             "Image '%s'\nx:%d, y:%d row:%d, bit-depth: %d, size: %d.\n",
+             img_path_fq, w, h, row, bpp, num_px
+    ) ;
+    
+    for( int i=0; i < 16; i++ ) {
+        Uint8 pix = *((Uint8*) bmp->pixels + i) ;
+        printf( "%d, ", pix ) ;
+    }
+    printf("\n") ;
+    
+    // data from cc
+    vision::tROIvec regions ;
+    vision::tROIset gutters ;
+
+    unsigned char threshold = 136 ;
+
+    // Profile connected components 
+    timespec tm_start, tm_end, tm_delta ;   
+
+    clock_gettime( CLOCK_PROCESS_CPUTIME_ID, &tm_start ) ;
+
+    // Test 1, just the whole image
+    regions = vision::ConnectedComponents( bmp->pixels, w, h, 0, num_px, threshold, 50, gutters ) ;
+
+    clock_gettime( CLOCK_PROCESS_CPUTIME_ID, &tm_end ) ;
+
+    tm_delta = TimeDelta( tm_start, tm_end ) ;
+    fprintf( stdout, "Time %d:%d \n", tm_delta.tv_sec, tm_delta.tv_nsec ) ;
+
+    SDL_FreeSurface( bmp ) ;
+
+    SDL_Quit() ;
+    return 1 ;
 }
--- C/vision.cpp
+++ C/vision.cpp
@@ -1,17 +1,25 @@
 #ifndef __VISION__
 #define __VISION__
 
+// Testing NEON Vs nieve dark px skipping __TEST_NEON__ / __TEST_NORMAL__
+#define __TEST_NORMAL__
+
 #include <cmath>
 #include <vector>
 #include <set>
 #include <map>
 
+#include <arm_neon.h>
+
 namespace vision {
 
 struct simpleDet {
-	float_t x, y, r ;
-	uint8_t w, h ;
+    float_t x, y, r ;
+    uint8_t w, h ;
 } ;
+// type Helpers for ROIs
+typedef std::vector< simpleDet > tROIvec ;
+
 
 /*
  * x,y -> m,n
@@ -22,83 +30,133 @@ extern const uint16_t ROI_BIG_NUM = 65535 ;
 extern const uint16_t ROI_NO_ID   = 60666 ;
 
 struct simpleROI {
-	// Bounding Box
-	uint16_t bb_x, bb_y, bb_m, bb_n ;
-	// *Previous* Scanline
-	uint16_t sl_x, sl_m, sl_n ;
-	// Region's id
-	uint16_t id ;
-	
-	// functions
-	void reset() {
-		bb_x = bb_y = ROI_BIG_NUM ;
-		bb_m = bb_n = 0 ;
-		sl_x = sl_m = sl_n = 0 ;
-		id = ROI_NO_ID ;
-	}
-	
-	void update() {
-		bb_x = std::min( bb_x, sl_x ) ;
-		bb_y = std::min( bb_y, sl_n ) ;
-		bb_m = std::max( bb_m, sl_m ) ;
-		bb_n = std::max( bb_n, sl_n ) ;
-	}
-	
-	void takeSL( simpleROI &other ) {
-		sl_x = other.sl_x ;
-		sl_m = other.sl_m ;
-		sl_n = other.sl_n ;
-	}
-	
-	void merge( simpleROI &other ) {
-		// BB box
-		bb_x = std::min( bb_x, other.bb_x ) ;
-		bb_y = std::min( bb_y, other.bb_y ) ;
-		bb_m = std::max( bb_m, other.bb_m ) ;
-		bb_n = std::max( bb_n, other.bb_n ) ;
-		// scanline
-		sl_x = other.sl_x ;
-		sl_m = other.sl_m ;
-		sl_n = other.sl_n ;
-		// take oldest id
-		id = std::min( id, other.id ) ;
-	}
+    // Bounding Box
+    uint16_t bb_x, bb_y, bb_m, bb_n ;
+    // *Previous* Scanline
+    uint16_t sl_x, sl_m, sl_n ;
+    // Region's id
+    uint16_t id ;
+    
+    // functions
+    void reset() {
+        bb_x = bb_y = ROI_BIG_NUM ;
+        bb_m = bb_n = 0 ;
+        sl_x = sl_m = sl_n = 0 ;
+        id = ROI_NO_ID ;
+    }
+    
+    void update() {
+        bb_x = std::min( bb_x, sl_x ) ;
+        bb_y = std::min( bb_y, sl_n ) ;
+        bb_m = std::max( bb_m, sl_m ) ;
+        bb_n = std::max( bb_n, sl_n ) ;
+    }
+    
+    void takeSL( simpleROI &other ) {
+        sl_x = other.sl_x ;
+        sl_m = other.sl_m ;
+        sl_n = other.sl_n ;
+    }
+    
+    void merge( simpleROI &other ) {
+        // BB box
+        bb_x = std::min( bb_x, other.bb_x ) ;
+        bb_y = std::min( bb_y, other.bb_y ) ;
+        bb_m = std::max( bb_m, other.bb_m ) ;
+        bb_n = std::max( bb_n, other.bb_n ) ;
+        // scanline
+        sl_x = other.sl_x ;
+        sl_m = other.sl_m ;
+        sl_n = other.sl_n ;
+        // take oldest id
+        id = std::min( id, other.id ) ;
+    }
 } ; // struct simpleROI
 
+// type Helpers for ROIs
+typedef std::vector< simpleROI >        tROIvec ;
+typedef std::set< simpleROI >           tROIset ;
+typedef std::map< uint16_t, uint16_t >  tROImap ;
+
 int sum( int a, int b ) {
-	return a + b ;
+    return a + b ;
 }
 
-std::vector<simpleROI> connectedComponents( unsigned char &img,
-											const uint16_t w,
-											const uint16_t h, 
-											const uint16_t img_in,
-											const uint16_t img_out,
-											const unsigned char threshold )
+tROIvec ConnectedComponents( unsigned char  &img,           // image to analyse
+                             const uint16_t w,              // img width
+                             const uint16_t h,              // img height
+                             const uint16_t img_in,         // Start idx of the 'slice' to analyse (inclusive)
+                             const uint16_t img_out,        // End idx of the 'slice' to analyse (exclusive)
+                             const unsigned char threshold, // Blob Brightness
+                             // returns
+                             tROIset &gutterballs,          // Regions that touch the first or last row, might merge
+)
 {
-	size_t 						idx = 0 ; 			// index into img
-	size_t 						reg_idx = 0 ; 		// index of 1st reg we might examine
-	size_t 						reg_start = 0 ; 	// first region that might touch current scanline
-	std::vector<simpleROI> 		reg_list ; 			// list of ROIs
-	std::map<uint16_t,uint16_t> reg_lut ;			// Map of ROI.id that need to merge
-	size_t						last_y = 0 ;		// last y we encountered
-	size_t						row_end = 0 ;		// idx of the end of the row we're scanning
-	size_t						tmp_x, tmp_y ;		// temp x,y of a bright pixel
-	simpleROI					tmp_reg ;			// temp region for comparison and merging
-	simpleROI					master_line ;		// master region scanline
-	simpleROI					scanline ; 			// current scanline
-	simpleROI					merge_target ; 		// temp space for merging regions
-	bool						ended = 0 ;			// have we finished looking at this slice
-	bool						skipping = 0 ; 		// are we trying to skip the predominantly dark regions
-	bool 						scanning = 0 ;		// are we scanning bright pix
-	bool 						inserting = 0 ;		// are we placing a ROI into the list
-	bool						merging = 0 ;		// are we merging to an existing ROI
-	
-	// TODO: solve connected components
-	
-	return reg_list ;
+    size_t      idx       = 0 ;     // index into img
+    size_t      reg_idx   = 0 ;     // index of 1st reg we might examine
+    size_t      reg_start = 0 ;     // first region that might touch current scanline
+    size_t      last_y    = 0 ;     // last y we encountered
+    size_t      row_end   = 0 ;     // idx of the end of the row we're scanning
+    size_t      tmp_x, tmp_y ;      // temp x,y of a bright pixel
+    size_t      gut_y, gut_n ;      // rows that are gutters
+
+    simpleROI   tmp_reg ;           // temp region for comparison and merging
+    simpleROI   master_line ;       // master region scanline
+    simpleROI   scanline ;          // current scanline
+    simpleROI   merge_target ;      // temp space for merging regions
+
+    bool        ended     = 0 ;     // have we finished looking at this slice
+    bool        skipping  = 0 ;     // are we trying to skip the predominantly dark regions
+    bool        scanning  = 0 ;     // are we scanning bright pix
+    bool        inserting = 0 ;     // are we placing a ROI into the list
+    bool        merging   = 0 ;     // are we merging to an existing ROI
+    
+    tROIvec     reg_vec ;           // vector of ROIs
+    tROImap     reg_lut ;           // Map of ROI.id that need to merge
+    tROImap     mergers ;           // Regions that have merged (Debug)
+
+    idx   = img_in ;
+    gut_y = img_in  / w ;
+    gut_n = img_out / w ;
+
+    // TODO: solve connected components
+
+    #ifdef __TEST_NORMAL__
+    // Test 1, just skip pix
+    while( img[ idx ] <= threshold ) {
+        ++idx ;
+    }
+    #endif
+
+    #ifdef __TEST_NEON__
+    // test 2, NEON
+    uint8x16_t threshold_vector = vdupq_n_u8( threshold ) ;
+    uint8x16_t mask_vector ;
+    uint8x16_t img_slice   ;
+    uint8x8_t  merge_bits  ;
+    uint8_t    any_above = 0 ;
+    while( !any_above ) {
+        // load Img slice
+        img_slice = (uint8x16_t *) img[ idx ] ;
+        // test for greatness - this should optimize into NEON, right?
+        mask_vector = img_slice > threshold_vector ;
+
+        // https://stackoverflow.com/questions/15389539/fastest-way-to-test-a-128-bit-neon-register-for-a-value-of-0-using-intrinsics
+        merge_bits = vorr_u8( vget_low_u8( mask_vector ), vget_high_u8( mask_vector ) ) ;
+        any_above  = vget_lane_u8( vpmax_u8( merge_bits, merge_bits ), 0 ) ;
+        idx += 16 ;
+    }
+    idx -= 16 ; // be nice to get idx of fist non-zero somehow
+    while( img[ idx ] <= threshold ) {
+        ++idx ;
+    }
+    #endif
+
+
+    printf( "hot px found at %d.\n", idx ) ;
+    return reg_list ;
 }
-											
+                                            
 
 
 } // namespace vision
