d4f68d17ecfe910c506e7fb3dba3d8555a4298c2
bit-meddler <RichH@Beacon-Optical.co.uk>
2021-05-22 09:58:19 +0100

    Fixed VS's Ugly formatting

--- C/tests.cpp
+++ C/tests.cpp
@@ -15,15 +15,15 @@ extern const line_t ROI_BIG_NUM = 65535;
 extern const line_t ROI_NO_ID   = 60666;
 
 struct scanline {
-	line_t x; // first hot pixel
-	line_t m; // last hot pixel
-	union { // the line's y
-		line_t y;
-		line_t n;
-	};
+	line_t x ; // first hot pixel
+	line_t m ; // last hot pixel
+	union {   // the line's y
+		line_t y ;
+		line_t n ;
+	} ;
 
 	inline void reset() {
-		x = m = n = 0;
+		x = m = n = 0 ;
 	}
 
 	inline bool touches( scanline &other ) {
@@ -33,28 +33,28 @@ struct scanline {
 			   (other.x >= x && other.x <= m) || \
 			   (other.m >= x && other.m <= m) ;
 	}
-};
+} ;
 
 struct simpleROI {
 	// Bounding Box 4
-	line_t bb_x, bb_y, bb_m, bb_n;
+	line_t bb_x, bb_y, bb_m, bb_n ;
 	// RoI ID 1
-	line_t id;
+	line_t id ;
 	// Last hot line 3
-	scanline sl;
+	scanline sl ;
 
 	inline void reset() {
-		bb_x = bb_y = ROI_BIG_NUM;
-		bb_m = bb_n = 0;
-		sl.x = sl.m = sl.n = 0;
-		id = ROI_NO_ID;
+		bb_x = bb_y = ROI_BIG_NUM ; 
+		bb_m = bb_n = 0 ;
+		sl.x = sl.m = sl.n = 0 ;
+		id = ROI_NO_ID ;
 	}
 
 	inline void update() {
-		bb_x = std::min(bb_x, sl.x);
-		bb_y = std::min(bb_y, sl.y);
-		bb_m = std::max(bb_m, sl.m);
-		bb_n = std::max(bb_n, sl.n);
+		bb_x = std::min( bb_x, sl.x ) ;
+		bb_y = std::min( bb_y, sl.y ) ;
+		bb_m = std::max( bb_m, sl.m ) ;
+		bb_n = std::max( bb_n, sl.n ) ;
 	}
 
 	inline void takeScanLine( scanline &other ) {
@@ -66,19 +66,19 @@ struct simpleROI {
 	}
 
 	inline void merge( simpleROI &other ) {
-		bb_x = std::min(bb_x, other.bb_x);
-		bb_y = std::min(bb_y, other.bb_y);
-		bb_m = std::max(bb_m, other.bb_m);
-		bb_n = std::max(bb_n, other.bb_n);
-		sl = other.sl;
+		bb_x = std::min( bb_x, other.bb_x ) ;
+		bb_y = std::min( bb_y, other.bb_y ) ;
+		bb_m = std::max( bb_m, other.bb_m ) ;
+		bb_n = std::max( bb_n, other.bb_n ) ;
+		sl = other.sl ;
 		// take oldest ID
-		id = std::min(id, other.id);
+		id = std::min( id, other.id ) ;
 	}
 
 	inline line_t area() {
-		return (bb_m - bb_x) * (bb_n - bb_y);
+		return (bb_m - bb_x) * (bb_n - bb_y) ;
 	}
-};
+} ;
 
 // type Helpers for ROIs
 typedef std::vector< simpleROI >               RoiVec_t ;
@@ -87,8 +87,8 @@ typedef std::map< line_t, line_t >             RoiIdMap_t ;
 typedef std::map< line_t, line_t >::iterator   RoiIdIt_t ;
 
 // debug
-void printLine(const scanline X) {
-	printf("x%d, y%d, m%d, n%d", X.x, X.y, X.m, X.n);
+void printLine( const scanline X ) {
+	printf( "x%d, y%d, m%d, n%d", X.x, X.y, X.m, X.n ) ;
 }
 
 RoiVec_t ConnectedComponents(
@@ -112,37 +112,37 @@ RoiVec_t ConnectedComponents(
 
 	// Regions & Region list stuff
 	// TODO: should be 2 vectors, val and ref, and I should manipulate the refs
-	RoiVec_t region_list ;        // the list of ROIs we have found
-	size_t   reg_idx = 0 ;        // index of 1st reg we might examine
-	size_t   reg_len = 0 ;        // length of region list
-	line_t   reg_id  = 0 ;        // Rolling auto ID of Regions
+	RoiVec_t region_list ;           // the list of ROIs we have found
+	size_t   reg_idx = 0 ;           // index of 1st reg we might examine
+	size_t   reg_len = 0 ;           // length of region list
+	line_t   reg_id  = 0 ;           // Rolling auto ID of Regions
 	auto stupid = region_list.begin(); // WTF is this shit, you can't insert with an int index fuck you c++17
 	// Region housekeeping to find join candidates
-	size_t   reg_start = 0 ;      // first region that might touch current scanline
-	size_t   reg_scan  = 0 ;      // used to split region_list
-	line_t   previous_y = 0;      // used to test of touchable regions
-	region_list.reserve(vec_size);
+	size_t   reg_start = 0 ;         // first region that might touch current scanline
+	size_t   reg_scan  = 0 ;         // used to split region_list
+	line_t   previous_y = 0;         // used to test of touchable regions
+	region_list.reserve(vec_size) ;
 
 	// Line detection
-	scanline    current_line ;    // temp scanline
+	scanline    current_line ;       // temp scanline
 
 	// Region joining
-	scanline    master_line ;     // master region scanline - for joining
-	line_t      master_line_id ;  // Region ID of the masterline
-	simpleROI   tmp_reg ;         // temp region for comparison and merging
-	simpleROI*  merge_target ;    // temp space for merging regions
-	RoiIdMap_t  merge_map ;       // Map of RoI.ids that need to merge
+	scanline    master_line ;        // master region scanline - for joining
+	line_t      master_line_id ;     // Region ID of the masterline
+	simpleROI   tmp_reg ;            // temp region for comparison and merging
+	simpleROI*  merge_target ;       // temp space for merging regions
+	RoiIdMap_t  merge_map ;          // Map of RoI.ids that need to merge
 
 	// Ejection control
-	bool        ended = 0;     // have we finished looking at this slice
+	bool        ended = 0 ;          // have we finished looking at this slice
 
 	// Gutters
 	const line_t gut_y = img_in  / img_w ; // top of image slice
 	const line_t gut_n = (img_out-1) / img_w ; // bottom of image slice
 
     // Guards
-	const uint8_t* img_in_ptr = &img[img_in] ;   // pointer to the 1st pxiel in the slice
-	const uint8_t* img_out_ptr = &img[img_out] ; // pointer to the terminus pxiel in the slice
+	const uint8_t* img_in_ptr  = &img[ img_in ]  ; // pointer to the 1st pxiel in the slice
+	const uint8_t* img_out_ptr = &img[ img_out ] ; // pointer to the terminus pxiel in the slice
 
 	// Setup for scanning
 	img_idx = img_in;
@@ -150,71 +150,70 @@ RoiVec_t ConnectedComponents(
 	uint8_t* img_ptr = &img[img_in] ; // img pointer that scans the image
 
 	// initalize structs
-	current_line.reset();
-	master_line.reset();
-	tmp_reg.reset();
+	current_line.reset() ;
+	master_line.reset() ;
+	tmp_reg.reset() ;
 
-	while (!ended) {
+	while( !ended ) {
 		// Skip dark pixels
-		while (*img_ptr < threshold && img_ptr < img_out_ptr) {
-			++img_ptr;
+		while( *img_ptr < threshold && img_ptr < img_out_ptr ) {
+			++img_ptr ;
 		}
-		if (img_ptr >= img_out_ptr) {
-			ended = 1;
-			break;
+		if( img_ptr >= img_out_ptr ) {
+			ended = 1 ;
+			break ;
 		}
 		// Bright px
-		img_idx = img_ptr - img_in_ptr;
+		img_idx = img_ptr - img_in_ptr ;
 
 		//printf("hot px found at idx:%d (x%d, y%d).\n", img_idx, img_idx % img_w, img_idx / img_w);
 
 		// scan this line
-		current_line.reset();
+		current_line.reset() ;
 
 		// scanline housekeeping
-		if (img_idx > row_end) {
+		if( img_idx > row_end ) {
 			// we're on a new row
-			tmp_y = img_idx / img_w;
-			tmp_x = img_idx % img_w;
-			row_start = tmp_y * img_w;
-			row_end = ((tmp_y + 1) * img_w) - 1;
-		}
-		else {
+			tmp_y = img_idx / img_w ;
+			tmp_x = img_idx % img_w ;
+			row_start = tmp_y * img_w ;
+			row_end = ((tmp_y + 1) * img_w) - 1 ;
+		} else {
 			// same row as last scanline
-			tmp_x = img_idx - row_start;
+			tmp_x = img_idx - row_start ;
 		}
 
-		current_line.x = tmp_x;
-		current_line.y = tmp_y;
+		current_line.x = tmp_x ;
+		current_line.y = tmp_y ;
 
 		// region list housekeeping
 		// There must be a bug in here somewhere
-		if (tmp_y > last_y) {
+		if( tmp_y > last_y ) {
 			// "left slice" have sl_n < last_y so can't join into the new scanline
 			// "Right slice" could join this or future scanlines on this y
-			reg_scan = reg_start;
-			reg_idx = reg_start;
-			reg_len = region_list.size();
-			previous_y = tmp_y - 1; // y above current
-			while (reg_scan < reg_len) {
-				if (region_list[reg_scan].sl.y < previous_y) {
+			reg_scan = reg_start ;
+			reg_idx = reg_start ;
+			reg_len = region_list.size() ;
+			previous_y = tmp_y - 1 ; // y above current
+			while( reg_scan < reg_len ) {
+				if( region_list[reg_scan].sl.y < previous_y ) {
 					// move reg_scan to the left
-					std::swap( region_list[reg_idx], region_list[reg_scan] ) ;
+					std::swap( region_list[ reg_idx ], region_list[ reg_scan ] ) ;
 					++reg_idx ;
 				}
 				++reg_scan ;
 			}
-			reg_start = reg_idx; // unreachable regions are moved to the left of start
-			last_y = tmp_y;
+			reg_start = reg_idx ; // unreachable regions are moved to the left of start
+			last_y = tmp_y ;
 		} // if y changed
 
 		// scan bright pixels, break if we go off the end of the row
-		while (row_end >= img_idx && *img_ptr >= threshold) {
-			++img_idx;
-			++img_ptr;
+		while( row_end >= img_idx && *img_ptr >= threshold ) {
+			++img_idx ;
+			++img_ptr ;
 		}
-		if (img_idx >= img_out) {
-			ended = 1; // off the end of the image, mark ended
+		if( img_idx >= img_out ) {
+			ended = 1 ; // off the end of the image, mark ended
 		}
 
 
@@ -230,47 +229,47 @@ RoiVec_t ConnectedComponents(
 		current_line.m = (img_idx - row_start) - 1;
 		//printf("ri %d, rl %d\n", reg_idx, reg_len);
 		if( (reg_idx == reg_len) || // 1st region 
-			(current_line.m < region_list[reg_idx].sl.x) // before a potential join
+			(current_line.m < region_list[ reg_idx ].sl.x) // before a potential join
 		  ) { 
 			// Insert a new RoI
 			//printf("New Region, can't touch\n");
-			simpleROI new_reg;
-			new_reg.reset();
-			new_reg.id = reg_id++;
-			new_reg.takeScanLine(current_line);
-			new_reg.update();
-			stupid = region_list.begin() + reg_idx;
-			region_list.insert(stupid, new_reg);
-			reg_len = region_list.size();
-			reg_idx += 1;
+			simpleROI new_reg ;
+			new_reg.reset() ;
+			new_reg.id = reg_id++ ;
+			new_reg.takeScanLine( current_line ) ;
+			new_reg.update() ;
+			stupid = region_list.begin() + reg_idx ;
+			region_list.insert( stupid, new_reg ) ;
+			reg_len = region_list.size() ;
+			++reg_idx ;
 		} else {
 			// check for potential joins
 
 			// skip regions that can't touch
-			while (reg_idx < reg_len && region_list[reg_idx].sl.m < current_line.x) {
+			while( reg_idx < reg_len && region_list[ reg_idx ].sl.m < current_line.x ) {
 				++reg_idx;
 			}
 			// x-m   x----m   xm
 			// 132456789123456789
 			//  x-----m x--m x--m
 
-			if( current_line.touches( region_list[reg_idx].sl ) ) {
+			if( current_line.touches( region_list[ reg_idx ].sl ) ) {
 				//printf("Touching region %d\n", reg_idx);
 				// touching regions
-				tmp_reg.reset();
-				tmp_reg.takeScanLine(current_line);
-				tmp_reg.update();
-				master_line    = region_list[reg_idx].sl;
-				master_line_id = region_list[reg_idx].id;
+				tmp_reg.reset() ;
+				tmp_reg.takeScanLine( current_line ) ;
+				tmp_reg.update() ;
+				master_line    = region_list[ reg_idx ].sl ;
+				master_line_id = region_list[ reg_idx ].id ;
 
-				region_list[reg_idx].merge(tmp_reg);
+				region_list[ reg_idx ].merge( tmp_reg ) ;
 
-				merge_target = &region_list[reg_idx];
+				merge_target = &region_list[ reg_idx ] ;
 				// are there subsequent regions to join?
-				reg_idx++;
-				while( reg_idx < reg_len && current_line.touches( region_list[reg_idx].sl)  ) {
+				++reg_idx ;
+				while( reg_idx < reg_len && current_line.touches( region_list[ reg_idx ].sl )  ) {
 					// 'merge_target' consumes the subsequent regions
-					merge_target->merge( region_list[reg_idx] );
+					merge_target->merge( region_list[ reg_idx ] ) ;
 
 					// remove the consumed region
 					stupid = region_list.begin() + reg_idx ;
@@ -278,27 +277,27 @@ RoiVec_t ConnectedComponents(
 					reg_len = region_list.size() ;
 				}
 				// Take back one khadam to honor the Hebrew God, whose Ark this is
-				reg_idx--; // go back a region in case subsequent lines touch
+				reg_idx-- ;
 				
 			} else {
 				// insert between regions
 				//printf("Between, might merge\n");
-				simpleROI new_reg;
-				new_reg.reset();
-				new_reg.id = reg_id++;
-				new_reg.takeScanLine(current_line);
-				new_reg.update();
-				stupid = region_list.begin() + reg_idx;
-				region_list.insert(stupid, new_reg);
-				reg_len = region_list.size();
-				reg_idx += 1;
-				if (current_line.touches(master_line) && current_line.y == master_line.y + 1) {
+				simpleROI new_reg ;
+				new_reg.reset() ;
+				new_reg.id = reg_id++ ;
+				new_reg.takeScanLine( current_line ) ;
+				new_reg.update() ;
+				stupid = region_list.begin() + reg_idx ;
+				region_list.insert( stupid, new_reg ) ;
+				reg_len = region_list.size() ;
+				++reg_idx ;
+				if( current_line.touches( master_line ) && current_line.y == (master_line.y + 1) ) {
 					// This region underhangs a masterline, map the masterline's id to the region
 					//printf("Masterline\n");
 					merge_map.insert(
-						std::pair<line_t, line_t>(
-							std::max(master_line_id, new_reg.id),
-							std::min(master_line_id, new_reg.id)
+						std::pair< line_t, line_t >(
+							std::max( master_line_id, new_reg.id ),
+							std::min( master_line_id, new_reg.id )
 						)
 					) ;
 				}
@@ -309,8 +308,8 @@ RoiVec_t ConnectedComponents(
 
 	// Tidy regions, flag gutters
 	// TODO: How to merge and remove RoIs?
-	for (RoiIdIt_t it = merge_map.begin(); it != merge_map.end(); ++it) {
-		printf("region id '%d' needs to merge into id '%d'\n", it->first, it->second) ;
+	for( RoiIdIt_t it = merge_map.begin(); it != merge_map.end(); ++it ) {
+		printf( "region id '%d' needs to merge into id '%d'\n", it->first, it->second ) ;
 	}
 	for( size_t i = 0; i < region_list.size(); i++ ) {
 		tmp_reg = region_list[i] ;
@@ -318,70 +317,70 @@ RoiVec_t ConnectedComponents(
 			gutterballs.insert( tmp_reg.id ) ;
 		}
 	}
-	return region_list;
+	return region_list ;
 }
 
 
 void test_cc() {
 	uint8_t test_img[] = \
 	{ //0    1    2    3    4    5    6    7    8    9   10   11      
-		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 0
-			0, 0, 0, 0, 60, 0, 0, 0, 0, 0, 0, 0, // 1 
-			0, 60, 60, 0, 0, 0, 0, 0, 40, 60, 40, 0, // 2
-			0, 60, 60, 0, 0, 60, 60, 0, 60, 70, 60, 0, // 3
-			0, 0, 0, 0, 0, 60, 60, 0, 40, 60, 40, 0, // 4
-			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 5
-			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 60, 0, // 6
-			0, 0, 0, 0, 0, 0, 60, 60, 60, 60, 60, 60, // 7
-			0, 0, 0, 0, 0, 0, 60, 60, 60, 0, 60, 0, // 8
-			0, 0, 0, 0, 0, 0, 60, 60, 60, 0, 0, 0, // 9
-			0,60,60, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 10
-			0,60,60, 0, 0, 0, 0, 0, 0, 0, 0, 0  // 11
-	}; // 12x12
+		0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0, // 0
+		0,   0,   0,   0,  60,   0,   0,   0,   0,   0,   0,   0, // 1 
+		0,  60,  60,   0,   0,   0,   0,   0,  40,  60,  40,   0, // 2
+		0,  60,  60,   0,   0,  60,  60,   0,  60,  70,  60,   0, // 3
+		0,   0,   0,   0,   0,  60,  60,   0,  40,  60,  40,   0, // 4
+		0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0, // 5
+		0,   0,   0,   0,   0,   0,   0,   0,   0,   0,  60,   0, // 6
+		0,   0,   0,   0,   0,   0,  60,  60,  60,  60,  60,  60, // 7
+		0,   0,   0,   0,   0,   0,  60,  60,  60,   0,  60,   0, // 8
+		0,   0,   0,   0,   0,   0,  60,  60,  60,   0,   0,   0, // 9
+		0,  60,  60,   0,   0,   0,   0,   0,   0,   0,   0,   0, // 10
+		0,  60,  60,   0,   0,   0,   0,   0,   0,   0,   0,   0  // 11
+	} ; // 12x12
 
-	for (int i = 0; i < 12; i++) {
-		for (int j = 0; j < 12; j++)
-			printf("%2d,", test_img[(i * 12) + j]);
-		printf("\n");
+	for( int i = 0; i < 12; i++ ) {
+		for( int j = 0; j < 12; j++ )
+			printf( "%3d,", test_img[ (i * 12) + j ] ) ;
+		printf( "\n" ) ;
 	}
-	uint8_t* image_data = &test_img[0];
-	RoiIdSet_t gutters;
-	uint8_t  threshold = 50;
-	RoiVec_t regions;
+	uint8_t*    image_data = &test_img[0] ; 
+	RoiIdSet_t  gutters ;
+	uint8_t     threshold = 50 ;
+	RoiVec_t    regions ;
 
-	regions = ConnectedComponents(image_data, 12, 12, 0, 144, threshold, 5, gutters);
+	regions = ConnectedComponents( image_data, 12, 12, 0, 144, threshold, 5, gutters ) ;
 
-	for (size_t i = 0; i < regions.size(); i++) {
-		simpleROI R = regions[i];
-		printf("{%d} %d: x%d, y%d, m%d, n%d\n", i, R.id, R.bb_x, R.bb_y, R.bb_m, R.bb_n);
+	for( size_t i = 0; i < regions.size(); i++ ) {
+		simpleROI R = regions[i] ;
+		printf( "{%d} %d: x%d, y%d, m%d, n%d\n", i, R.id, R.bb_x, R.bb_y, R.bb_m, R.bb_n ) ;
 	}
-	printf("These RoI ids are gutterballs: ");
-	std::set<line_t>::iterator it;
+	printf( "These RoI ids are gutterballs: " ) ;
+	std::set<line_t>::iterator it ;
 	for( it = gutters.begin(); it != gutters.end(); ++it ) {
 		printf( "%d ", *it ) ;
 	}
 }
 
 void test_structs() {
-	scanline X;
-	X.x = 1;
-	X.y = 2;
-	X.m = 3;
-	X.n = 4;
+	scanline X ;
+	X.x = 1 ;
+	X.y = 2 ;
+	X.m = 3 ;
+	X.n = 4 ;
 
-	printf("x%d, y%d, m%d, n%d\n", X.x, X.y, X.m, X.n);
+	printf( "x%d, y%d, m%d, n%d\n", X.x, X.y, X.m, X.n ) ;
 
-	simpleROI Y;
-	Y.reset();
-	Y.takeScanLine(X);
-	Y.update();
+	simpleROI Y ;
+	Y.reset() ;
+	Y.takeScanLine( X ) ;
+	Y.update() ;
 
-	printf("BB: x%d, y%d, m%d, n%d SL:x%d, m%d, n%d\n",
-		Y.bb_x, Y.bb_y, Y.bb_m, Y.bb_n,
-		Y.sl.x, Y.sl.m, Y.sl.n);
+	printf( "BB: x%d, y%d, m%d, n%d SL:x%d, m%d, n%d\n",
+		    Y.bb_x, Y.bb_y, Y.bb_m, Y.bb_n,
+		    Y.sl.x, Y.sl.m, Y.sl.n ) ;
 }
 
 void main() {
-	test_cc();
-
+	test_cc() ;
 }
+
